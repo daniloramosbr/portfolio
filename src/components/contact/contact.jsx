bbcf72208d1e78873c61208e178f72a263a6fd46
Danilo Ramos <125937280+daniloramosbr@users.noreply.github.com>
2023-08-17 10:46:37 -0300

    att-contact

--- src/components/contact/contact.jsx
+++ src/components/contact/contact.jsx
@@ -9,19 +9,20 @@ export default function Contact() {
           <b>
             <ion-icon name="call-outline"></ion-icon> <span>Telefone:</span>
           </b>
-          <span>43 99172-7409</span>
+          <span> 43 99172-7409</span>
         </ol>
         <ol>
           <b>
             <ion-icon name="mail-outline"></ion-icon> <span>E-mail:</span>
           </b>
-          <span>daniloramoscr1905@gmail.com</span>
+          <span> daniloramoscr1905@gmail.com</span>
+        </ol>
+        <hr />
+        <ol>
+     <a href="https://www.linkedin.com/in/daniloramosbr"><button> <div> <ion-icon name="logo-linkedin"></ion-icon></div> <span>Linkedin</span></button></a>
         </ol>
         <ol>
-          <b>
-            <ion-icon name="logo-linkedin"></ion-icon> <span>Linkedin:</span>
-          </b>
-          <span>@daniloramosbr</span>
+      <a href="https://github.com/daniloramosbr"><button> <ion-icon name="logo-github"></ion-icon> <span>GitHub</span></button></a>
         </ol>
       </ul>
     </div>
